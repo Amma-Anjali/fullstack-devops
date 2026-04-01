59b16651f88e9976b64de8e71d79f539f4096fee
AMMA ANJALI <ammaanjali@AMMAs-MacBook-Air.local>
2026-04-01 15:25:48 +0530

    update

---
+++ backend/models/reservation.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const reservationSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  date: String,
+  time: String,
+});
+
+export const Reservation = mongoose.model(
+  "Reservation",
+  reservationSchema
+);
